50d6efcccdb376c9a15b9fcf2da9cee01f213209
MrHappyyy <v.yur4ik@gmail.com>
2016-05-04 17:23:05 +0300

    добавлена таблица копилки и добавлен клас события для статистики

---
+++ app/src/main/java/com/example/mrhappyyy/moneybox/database/EventEntity.java
@@ -0,0 +1,7 @@
+package com.example.mrhappyyy.moneybox.database;
+
+/**
+ * Created by mrhappyyy on 04.05.16.
+ */
+public class EventEntity {
+}
---
+++ app/src/main/java/com/example/mrhappyyy/moneybox/database/TableMoneyBox.java
@@ -0,0 +1,7 @@
+package com.example.mrhappyyy.moneybox.database;
+
+/**
+ * Created by mrhappyyy on 03.05.16.
+ */
+public class TableMoneyBox {
+}
